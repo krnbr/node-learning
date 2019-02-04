4b887c307f1d9a3eae13a50e44602f199b596053
Karanbir Singh <krnbr@live.in>
2019-02-04 20:53:33 +0530

    request router

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
 
     // GET PATH
     let path = parsedUrl.pathname;
-    let trimmedPath = path.replace(/\+|\/+$/g, '');
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
     // GET the QUERY STRING as an Object
     let queryStringObject = JSON.parse(JSON.stringify(parsedUrl.query));
@@ -29,17 +29,57 @@ const server = http.createServer((req, res) => {
     });
     req.on('end', () => {
         buffer += decoder.end();
-        // SEND the RESPONSE
-        res.end("Hello World\n");
+        /*// SEND the RESPONSE
+        res.end("Hello World\n");*/
 
-        // LOG the request
         console.log('Request is received on path: '+trimmedPath+' with method: '+method+' with the query strings ',queryStringObject);
-        console.log('Request received with headers ',headers);
-        console.log('Request received with payload ',buffer);
+        // choose the handler where it should go
+        // if found then go sample handler else not found
+        let matchedHandler = typeof(router[trimmedPath]) !== 'undefined' ? router['sample'] : handlers.notFound;
+
+        let data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload:buffer
+        };
+
+        matchedHandler(data, (statusCode, payload) => {
+            // LOG the request
+            console.log('Request is received on path: '+trimmedPath+' with method: '+method+' with the query strings ',queryStringObject);
+            console.log('Request received with headers ',headers);
+            console.log('Request received with payload ',buffer);
+
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            let payloadString = JSON.stringify(payload);
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
+
     });
 
 });
 
+
 server.listen(6982,
     () => console.log("The server is listening to port 6982 now")
 );
+
+// define handlers
+let handlers = {};
+
+handlers.sample = (data, callback) => {
+    // Callback a http status code, and a payload object
+    callback(406, {'name':'sample handler'});
+};
+
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// define a request router
+let router = {
+    'sample':handlers.sample
+};
